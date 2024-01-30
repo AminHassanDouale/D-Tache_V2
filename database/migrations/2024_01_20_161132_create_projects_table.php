c8fbe4c07a75fcdac2d08fb50ad0227d9d587c29
AminHassanDouale <amin.hassan@d-money.dj>
2024-01-30 10:31:40 +0300

    updated project migration

--- database/migrations/2024_01_20_161132_create_projects_table.php
+++ database/migrations/2024_01_20_161132_create_projects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->string('name');
             $table->text('description');
             $table->unsignedBigInteger('status_id');
