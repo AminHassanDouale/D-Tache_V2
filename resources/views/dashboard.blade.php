002a5be58516367e46ed4e5f8ec6616b29eb47d8
AminHassanDouale <amin.hassan@d-money.dj>
2024-02-16 04:02:48 +0300

    dashboard update_V0_1
    this is an update for dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,22 @@
 <x-app-layout>
+    <style>
+        @keyframes flash {
+        0% { opacity: 1; }
+        50% { opacity: 0; }
+        100% { opacity: 1; }
+    }
+    
+    .flash-alert {
+        animation: flash 1s infinite;
+    }
+    
+    </style>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+    
     <livewire:report.dashboard />
 
 </x-app-layout>
--- resources/views/livewire/report/dashboard.blade.php
+++ resources/views/livewire/report/dashboard.blade.php
@@ -3,6 +3,7 @@ use Livewire\Volt\Component;
 use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
 use Livewire\WithPagination;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 
 new class extends Component {
@@ -10,12 +11,13 @@ new class extends Component {
 
     public function tasks()
     {
-        return Task::whereHas('assignee', function (Builder $query) {
-                $query->where('department_id', Auth::user()->department_id);
+        return  Task::where(function($query) {
+                $query->where('user_id', Auth::user()->id)
+                      ->orWhere('assignee_id', Auth::user()->id);
             })
             ->where('status_id', '!=', 3) 
-            ->with(['project', 'status', 'assignee'])
-            ->paginate(10); 
+            ->orderBy('created_at', 'desc')
+            ->paginate(8);
     }
 
     public function with(): array
@@ -32,27 +34,66 @@ new class extends Component {
         <div class="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
             <p class="text-center sm:text-left">Bienvenue sur D-Tache, {{ Auth::user()->fullname }}. <span id="timing" class="block float-right sm:inline-block"></span></p>
             <br>
-            
-                <div class="overflow-hidden shadow-sm dark:bg-gray-800 sm:rounded-lg">
-                    <div class="p-6 text-gray-900 bg-white dark:text-gray-100">
+            <x-tabs selected="users-tab">
+                <x-tab name="users-tab" label="Tasks" icon="s-bars-3-center-left">
+                   
+                    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
                         @if ($tasks->isEmpty())
                             <p class="text-gray-500">No tasks found.</p>
                         @else
                             @foreach ($tasks as $task)
-                                <div class="p-4 mb-4 bg-white rounded-lg shadow ">
-                                    <h2 class="text-lg font-semibold">{{ $task->name ?: 'No Task Name' }}</h2>
-                                    <p class="text-sm text-gray-600">{{ $task->description ?: 'No Description' }}</p>
-                                    <p class="text-sm text-gray-500">Project: {{ optional($task->project)->name ?: 'No Project Name' }}</p>
-                                    <p class="text-sm text-gray-500">Status: {{ optional($task->status)->name ?: 'No Status' }}</p>
-                                    <p class="text-sm text-gray-500">Assignee: {{ optional($task->assignee)->name ?: 'No Assignee' }}</p>
+                                <!-- Pricing Card 1 -->
+                                <div class="overflow-hidden transition-transform transform bg-white rounded-lg shadow-lg hover:scale-105">
+                                    <div class="p-1 bg-blue-200">
+                                    </div>
+                                    <div class="p-8">
+                                        <h2 class="mb-4 text-3xl font-bold text-gray-800"></h2>
+                                        <p class="mb-6 text-gray-600"> <strong> {{ $task->name }} </strong></p>
+                                        <ul class="mb-6 text-sm text-gray-600">
+                                            <li class="flex items-center mb-2">
+                                                Status: 
+                                                <span class="{{ $task->status_id == 1 ? 'text-blue-500' : ($task->status_id == 2 ? 'text-orange-500' : 'text-green-500') }}">
+                                                    {{ $task->status_id ? $task->status->name : ''}}
+                                                </span>
+                                            </li><li class="flex items-center mb-2">
+                                                {{ $task->user->username }}
+                                            </li><li class="flex items-center mb-2">
+                                                {{ $task->assignee_id == Auth::user()->id ? 'Vous-même' : ($task->assignee ? $task->assignee->name : '') }}                                                 
+                                            </li>
+                                            
+                                            
+                                            <li class="flex items-center">
+                                                
+                                                <x-icon name="m-chat-bubble-bottom-center-text" /> {{ $task->comments->count() }}
+                                                <x-icon name="m-folder" /> {{ $task->files->count() }}
+                                            </li>
+                                            <li class="flex items-center mb-2">
+                                                Due Date: 
+                                                <span class="due-date {{ Carbon::now()->gt($task->due_date) ? 'text-red-500' : '' }}">
+                                                    {{ $task->due_date }}
+                                                </span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="p-4"><a href="{{ route('tasks.show', $task) }}" >
+                                        <button class="w-full px-4 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+                                            Voir
+                                        </button>
+                                    </a>
+                                    </div>
                                 </div>
                             @endforeach
                             {{ $tasks->links() }}
                         @endif
                     </div>
-                </div>
-                
-      
+                </x-tab>
+                <x-tab name="tricks-tab" label="Tricks" icon="o-sparkles">
+                    <div>Tricks</div>
+                </x-tab>
+                <x-tab name="musics-tab" label="Musics" icon="o-musical-note">
+                    <div>Musics</div>
+                </x-tab>
+            </x-tabs>
         </div>
     </div>
 </div>
@@ -72,5 +113,18 @@ new class extends Component {
     }
 
     updateTiming();
+</script>
+<script>
+    setInterval(function() {
+        var dueDates = document.querySelectorAll('.due-date');
+        dueDates.forEach(function(element) {
+            var dueDate = new Date(element.textContent);
+            if (dueDate < Date.now()) {
+                element.classList.add('flash-alert');
+            } else {
+                element.classList.remove('flash-alert');
+            }
+        });
+    }, 1000); // Flash every 1 second
 </script>
 
